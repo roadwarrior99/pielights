7ce4d4f2d2553288a70f3775889224c9c3c8fc82
roadwarrior99 <colinmhayes@gmail.com>
2017-02-05 17:21:57 -0600

    request saves

--- PieService.php
+++ PieService.php
@@ -8,13 +8,19 @@
         {
             $req = json_decode($_POST("body"),true);
             if($req!=null){
-                //Handle Request                       
+                //Handle Request
+                
+                $voice="";
+                $resp="";
+                $parms = array(new dbParameter("@voice",$voice),new dbParameter("@response",$resp));
+                $db->nonQueryParm("insert into request(voiceQuery,response) values('@voice','@response');");    
             }
         }
      } catch (Exception $e)
       {
         $parms = array(new dbParameter('@error',$e));
-        $db.nonQueryParm("insert into error_log(error) values('@error')",$parms);
+        $db->nonQueryParm("insert into error_log(error) values('@error')",$parms);
         //echo 'Caught exception: ',  $e->getMessage(), "\n";
         }
+
 ?>
